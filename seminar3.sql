986b499f7477946c38f5b31816e51340df2c04a4
ISErmakov <119028263+ISErmakov@users.noreply.github.com>
2023-11-10 10:31:11 +0500

    Create seminar3.sql

---
+++ seminar3.sql
@@ -0,0 +1,75 @@
+DROP DATABASE IF EXISTS lesson3;
+CREATE DATABASE IF NOT EXISTS `lesson3`;
+USE `lesson3`;
+
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff`
+(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`firstname` VARCHAR(45),
+`lastname` VARCHAR(45),
+`post` VARCHAR(45),
+`seniority` INT,
+`salary` INT,
+`age` INT);
+
+INSERT INTO `staff` (`firstname`, `lastname`, `post`,`seniority`,`salary`, `age`)
+VALUES
+('Вася', 'Васькин', 'Начальник', 40, 100000, 60), 
+('Петр', 'Власов', 'Начальник', 8, 70000, 30),
+('Катя', 'Катина', 'Инженер', 2, 70000, 25),
+('Саша', 'Сасин', 'Инженер', 12, 50000, 35),
+('Иван', 'Петров', 'Рабочий', 40, 30000, 59),
+('Петр', 'Петров', 'Рабочий', 20, 55000, 60),
+('Сидр', 'Сидоров', 'Рабочий', 10, 20000, 35),
+('Антон', 'Антонов', 'Рабочий', 8, 19000, 28),
+('Юрий', 'Юрков', 'Рабочий', 5, 15000, 25),
+('Максим', 'Петров', 'Рабочий', 2, 11000, 19),
+('Юрий', 'Петров', 'Рабочий', 3, 12000, 24),
+('Людмила', 'Маркина', 'Уборщик', 10, 10000, 49);
+
+SELECT * FROM staff;
+
+DROP TABLE IF EXISTS `activity_staff`;
+CREATE TABLE IF NOT EXISTS `activity_staff`
+(`id` INT PRIMARY KEY AUTO_INCREMENT,
+`staff_id` INT,
+FOREIGN KEY (staff_id) REFERENCES staff(id),
+`date_activity` DATE,
+`count_pages` INT
+);
+
+
+INSERT activity_staff (`staff_id`, `date_activity`, `count_pages`)
+VALUES
+(1,'2022-01-01',250),
+(2,'2022-01-01',220),
+(3,'2022-01-01',170),
+(1,'2022-01-02',100),
+(2,'2022-01-01',220),
+(3,'2022-01-01',300),
+(7,'2022-01-01',350),
+(1,'2022-01-03',168),
+(2,'2022-01-03',62),
+(3,'2022-01-03',84);
+
+SELECT * FROM staff ORDER BY salary;
+
+SELECT * FROM staff ORDER BY salary DESC;
+
+SELECT * FROM staff ORDER BY salary DESC LIMIT 5;
+
+
+SELECT GROUP_CONCAT(lastname), post, SUM(salary) FROM staff GROUP BY post;
+
+SELECT count(post) from staff WHERE post like "рабочий" and age between 24 and 49;
+
+select count(post) from (SELECT DISTINCT post from staff) AS t;
+
+select post from staff group by post having avg(age)<= 30;
+
+select staff_id from activity_staff group by staff_id having sum(count_pages) > 500;
+
+select staff_id from activity_staff where date_activity in (select date_activity from activity_staff group by date_activity having count(staff_id) > 3);
+
+select avg(salary), post from staff group by post having avg(salary) > 30000;
